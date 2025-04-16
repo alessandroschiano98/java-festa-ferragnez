177f88b1cc05899aca38e809a214dd8d9afdb18d
alessandroschiano98 <schianoski98@gmail.com>
2025-04-16 21:02:22 +0200

    first commit

---
+++ src/CheckGuest.java
@@ -0,0 +1,10 @@
+public class CheckGuest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+    
+    /* LISTA INVITATI */
+    // Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi,
+    // Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic
+
+}
