8e3bee48cd9b87271ed31fa9cb30efb6dc8c8edb
alessandroschiano98 <schianoski98@gmail.com>
2025-04-17 17:15:58 +0200

    creata lista array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,12 +1,13 @@
 package com.ferragnez.party;
-
 public class CheckGuest {
     public static void main(String[] args) {
-        
 
+        // Scanner input = new Scanner(System.in);
 
+        String[] listainvitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+        System.out.println(listainvitati);
 
-
-        
+        // input.close();
     }
 }
