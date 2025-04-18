fac568e2d3bbf24f0cdd793133ea11d885568315
alessandroschiano98 <schianoski98@gmail.com>
2025-04-18 15:21:20 +0200

    correzioni

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -14,8 +14,12 @@ public class CheckGuest {
             "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" 
         };
 
+        // MESSAGGIO DI BENVENUTO
+        System.out.println("Benvenuto al Ferragnez-Party, mi dica il suo nominativo così che possa verificare se è un invitato. ");
+
         // L'UTENTE INSERISCE IL NOME
-        System.out.print("Inserisci il tuo nome: ");
+        
+        System.out.println("Inserisca il suo nome: ");
         String nomeInvitato = input.nextLine();
 
         boolean invitatoTrovato = false;
@@ -24,13 +28,12 @@ public class CheckGuest {
         for (int i = 0; i < listaInvitati.length; i++) {
             if (listaInvitati[i].equalsIgnoreCase(nomeInvitato)) {
                 invitatoTrovato = true;
-                break;
             }
         }
 
         // RISPOSTA
         if (invitatoTrovato) {
-            System.out.println("Benvenuto alla festa!");
+            System.out.println("Il suo nome è nella lista, si accomodi pure " + "Sign/Sig.ra " + nomeInvitato + ".");
         } else {
             System.out.println("Mi dispiace, non sei nella lista.");
         }
