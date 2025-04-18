5020d2b098db6884590229d50403da3846d768e4
alessandroschiano98 <schianoski98@gmail.com>
2025-04-18 15:08:44 +0200

    verifica effettuata

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,20 +1,40 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
 
-        // Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        String[] listainvitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
-                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
-        System.out.println("Lista invitati:");
+        // LISTA INVITATI
+        String[] listaInvitati = { 
+            "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", 
+            "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", 
+            "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" 
+        };
 
-        for (int i= 0; i < listainvitati.length; i++) {
-        
-            System.out.println(listainvitati[i]);
+        // L'UTENTE INSERISCE IL NOME
+        System.out.print("Inserisci il tuo nome: ");
+        String nomeInvitato = input.nextLine();
 
+        boolean invitatoTrovato = false;
+
+        // VERIFICARE SE IL NOME INSERITO E' IN LISTA
+        for (int i = 0; i < listaInvitati.length; i++) {
+            if (listaInvitati[i].equalsIgnoreCase(nomeInvitato)) {
+                invitatoTrovato = true;
+                break;
+            }
         }
 
-        // input.close();
+        // RISPOSTA
+        if (invitatoTrovato) {
+            System.out.println("Benvenuto alla festa!");
+        } else {
+            System.out.println("Mi dispiace, non sei nella lista.");
+        }
+
+        input.close();
     }
 }
