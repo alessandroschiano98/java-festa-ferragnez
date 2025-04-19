bb58f23381e261c9b6f17a759be0d4034b69ee83
alessandroschiano98 <schianoski98@gmail.com>
2025-04-19 13:22:02 +0200

    correzione (aggiunto break) motivo: anche se il codice parte lo stesso, in una situazione alternativa in cui i nomi sono migliaia, non mettere il break vorrebbe dire far scorrere tutti i nomi anche se quello che mi serve gia è stato trovato, dunque rallenterei il processo inutilmente.

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -8,26 +8,34 @@ public class CheckGuest {
         Scanner input = new Scanner(System.in);
 
         // LISTA INVITATI
-        String[] listaInvitati = { 
-            "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", 
-            "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", 
-            "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" 
+        String[] listaInvitati = {
+                "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax",
+                "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis",
+                "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"
         };
 
+        /* // SE AVESSI VOLUTO FAR USCIRE ANCHE LA LISTA VISIBILE
+         * for (int i = 0; i < listaInvitati.length; i++){
+         * System.out.println(listaInvitati[i]);
+         * }
+         */
+
         // MESSAGGIO DI BENVENUTO
-        System.out.println("Benvenuto al Ferragnez-Party, mi dica il suo nominativo così che possa verificare se è un invitato. ");
+        System.out.println(
+                "Benvenuto al Ferragnez-Party, mi dica il suo nominativo così che possa verificare se è un invitato. ");
 
         // L'UTENTE INSERISCE IL NOME
-        
         System.out.println("Inserisca il suo nome: ");
         String nomeInvitato = input.nextLine();
 
-        boolean invitatoTrovato = false;
+        boolean invitatoTrovato = false; // COSI' QUALSIASI NOME INSERITO DALL'UTENTE NON CORRISPONDA A QUELLA DEGLI
+                                         // ARRAY DARA' SEMPRE FALSO
 
         // VERIFICARE SE IL NOME INSERITO E' IN LISTA
         for (int i = 0; i < listaInvitati.length; i++) {
             if (listaInvitati[i].equalsIgnoreCase(nomeInvitato)) {
-                invitatoTrovato = true;
+                invitatoTrovato = true; // QUI INVECE SE IL NOME DELL'UTENTE INSERITO CORRISPONDE A QUELLO DELLA
+                break;                        // listainvitati[i] E' TRUE
             }
         }
 
